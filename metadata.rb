475da81004c2f1cee0f9d3ae10b834ed9471dd4d
Dmitry Malikov <color404051h@gmail.com>
2013-02-06 00:02:28 +0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,6 @@
 description      "Smackage installation with smlnj and mlton configuring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer       'Dmitry Malikov'
+maintainer_email 'malikov.d.y@gmail.com'
 version          "0.0.1"
+
